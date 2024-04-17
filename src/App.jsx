7b87c50023b942f0733856aed6b1d820ae3e8172
unknown <mnmsby4@gmail.com>
2024-04-17 19:06:46 +0530

    routs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-
+import React from "react"
+import { BrowserRouter , Routes, Route} from 'react-router-dom';
+import Home from "./pages/Home";
+import Login from "./pages/LoginPage"
+import Admin from "./pages/Admin";
 
 function App() {
 
   return (
     <>
-      <h1>Hai</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/'  element={<Home/>} />
+        <Route  path='/login'  element={<Login/>} />
+        <Route  path='/admin'  element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
